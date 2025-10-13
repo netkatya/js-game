27a4b6173c2ac8c0870b7033f6401f75560dae8f
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-10-13 14:21:37 +0100

    monaco added

---
+++ components/Monaco/MonacoEditor.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Editor from "@monaco-editor/react";
+
+interface MonacoEditorProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export default function MonacoEditor({ value, onChange }: MonacoEditorProps) {
+  return (
+    <Editor
+      height="400px"
+      language="javascript"
+      value={value}
+      onChange={(v) => onChange(v ?? "")}
+      theme="vs-dark"
+      options={{
+        minimap: { enabled: false },
+        fontSize: 14,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+        lineNumbers: "on",
+        wordWrap: "on",
+        cursorBlinking: "smooth",
+        readOnly: false,
+        folding: false,
+        glyphMargin: false,
+        renderValidationDecorations: "off",
+      }}
+    />
+  );
+}
