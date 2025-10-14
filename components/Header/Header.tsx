22d94ebf98c388c16217875eabbbbc87fb1fd243
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-10-14 13:34:16 +0100

    hover fixed

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,13 +7,13 @@ export default function Header() {
       <Image src="/logo.png" alt="logo of the website" width="80" height="80" />
       <nav>
         <ul className="flex gap-[30px] text-2xl font-semibold">
-          <li className="hover:underline">
+          <li className="text-[#079CDE] transition duration-300 hover:[text-shadow:0_0_10px_#079CDE]">
             <Link href="/">Home</Link>
           </li>
-          <li className="hover:underline">
+          <li className="text-[#079CDE] transition duration-300 hover:[text-shadow:0_0_10px_#079CDE]">
             <Link href="#">About A.R.I</Link>
           </li>
-          <li className="hover:underline">
+          <li className="text-[#079CDE] transition duration-300 hover:[text-shadow:0_0_10px_#079CDE]">
             <Link href="/levels">Levels</Link>
           </li>
           {/* <li className="hover:underline">
