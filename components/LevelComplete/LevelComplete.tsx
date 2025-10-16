a2d9515e3144b8c9448425cb6740c298ccde7ccb
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-10-16 17:31:34 +0100

    modif

--- components/LevelComplete/LevelComplete.tsx
+++ components/LevelComplete/LevelComplete.tsx
@@ -11,24 +11,34 @@ interface LevelCompleteProps {
 export default function LevelComplete({ level, route }: LevelCompleteProps) {
   const router = useRouter();
   return (
-    <main className="min-h-screen flex items-center justify-center p-4">
+    <main
+      className="flex items-center justify-center p-8
+             bg-[length:400%_400%]
+             animate-[neonGradient_12s_ease_infinite]
+             relative overflow-hidden"
+      style={{
+        backgroundImage:
+          "linear-gradient(315deg, #000017, #021940, #0b004f, #001c5e, #0ff, #001c5e, #0b004f, #021940, #000017)",
+      }}
+    >
       <div className="flex gap-4">
-        <div className="w-[370px] h-[500px] relative overflow-hidden rounded-2xl border-b-2 border-b-[#079CDE] shadow-[0_0_20px_#079CDE]">
-          <Image
-            src="/img/robot_level_comp.png"
-            width={370}
-            height={470}
-            alt="Robot celebrating"
+        <div className="w-[450px] h-[600px] relative overflow-hidden rounded-2xl border-b-2 border-b-[#079CDE] shadow-[0_0_20px_#079CDE]">
+          <video
+            src="/gif/robot_level.mp4"
+            autoPlay
+            loop
+            muted
+            playsInline
             className="absolute -top-12 left-0"
           />
         </div>
 
-        <div className="w-xl text-center p-8 rounded-lg shadow-lg">
+        <div className="w-xl text-center p-8">
           <div>
-            <div className="h-[200px] p-8">
+            <div className="h-[200px] pt-16 pb-24">
               <TextType
                 text={[
-                  `Well done, human!\nYour logic is impressive.\nAdvancing to the next level...`,
+                  `Well done, human! Your logic is\n impressive. Advancing to the next\n level...`,
                 ]}
                 typingSpeed={75}
                 pauseDuration={1500}
@@ -38,13 +48,13 @@ export default function LevelComplete({ level, route }: LevelCompleteProps) {
               />
             </div>
           </div>
-          <h1 className="text-4xl font-bold text-white mb-4">
+          <h1 className="text-4xl font-bold text-white mb-16">
             Level {level} Complete!
           </h1>
           <button
             onClick={() => router.push(`/quiz/${route}`)} // Змініть на правильний шлях
-            className="w-xm p-3 text-center border-3 border-solid border-[#079cde] rounded-md 
-                 transition-shadow duration-300 hover:shadow-[0_0_20px_#079CDE] text-xl"
+            className="w-[420px] min-h-[80px] p-3 text-center border-3 border-solid border-[#079cde] rounded-md 
+                 transition-shadow duration-300 hover:shadow-[0_0_20px_#079CDE] text-2xl font-semibold"
           >
             Go to the next Level
           </button>
