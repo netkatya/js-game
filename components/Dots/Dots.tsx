56da7719ad13a1bdbf3a62ea904fafd4ddcd579f
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-10-18 20:17:50 +0100

    fix

--- components/Dots/Dots.tsx
+++ components/Dots/Dots.tsx
@@ -5,15 +5,18 @@ import { InertiaPlugin } from "gsap/InertiaPlugin";
 
 gsap.registerPlugin(InertiaPlugin);
 
-const throttle = (func: (...args: any[]) => void, limit: number) => {
+const throttle = <T extends (...args: unknown[]) => void>(
+  func: T,
+  limit: number
+): T => {
   let lastCall = 0;
-  return function (this: any, ...args: any[]) {
+  return function (this: unknown, ...args: Parameters<T>) {
     const now = performance.now();
     if (now - lastCall >= limit) {
       lastCall = now;
       func.apply(this, args);
     }
-  };
+  } as T;
 };
 
 interface Dot {
