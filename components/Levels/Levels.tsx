e2250badcbab31234505bcb4700b85acdade9965
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-10-18 21:48:36 +0100

    modif

--- components/Levels/Levels.tsx
+++ components/Levels/Levels.tsx
@@ -4,6 +4,7 @@ export default function Levels() {
   return (
     <section className="pt-[60px] pb-[40px] flex justify-center" id="levels">
       <div className="pl-[20px] pr-[20px] max-w-[1200px]">
+        <h2 className="text-8xl text-center mb-[16px] font-semibold">Levels</h2>
         <ul className="flex flex-col gap-[80px]">
           <li className="flex gap-[184px] items-center">
             <Image
@@ -127,39 +128,3 @@ export default function Levels() {
     </section>
   );
 }
-
-// import Link from "next/link";
-
-// const availableLevels = [
-//   { id: 1, name: "Tests", route: "levelOne" },
-//   { id: 2, name: "React Fundamentals" },
-//   { id: 3, name: "React Fundamentals" },
-//   { id: 4, name: "React Fundamentals" },
-//   { id: 5, name: "React Fundamentals" },
-// ];
-
-// export default function Levels() {
-//   return (
-//     <div className="flex  items-center justify-center mb-[105px] flex-col p-[37px]">
-//       <h1 className="text-[128px] font-semibold mb-[25px]">Choose Level</h1>
-//       <div>
-//         <ul className="flex flex-col flex-wrap gap-[25px] items-center">
-//           {availableLevels.map((level) => (
-//             <li
-//               key={level.id}
-//               className="flex justify-center w-[567px] h-[126px] border-3 p-[12px] border-solid border-[#079cde] transition hover:translate-y-[-5px]"
-//             >
-//               <Link
-//                 href={`/quiz/${level.route}`}
-//                 className="text-[64px] font-semibold"
-//               >
-//                 {" "}
-//                 {level.name}
-//               </Link>
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// }
