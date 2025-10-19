03fac9f645e93a78bfefe28d91784d9f5211da3c
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-10-19 17:57:27 +0100

    modif

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -35,7 +35,7 @@ export default function Hero() {
   return (
     <section className="pt-[40px] md:pt-[80px] pb-[20px] md:pb-[60px]">
       <div className="flex items-center justify-center gap-[24px] md:gap-[40px] pl-[10px] pr-[10px] md:pl-[20px] md:pr-[20px]">
-        <div className="relative w-1/3 w-[180px] h-[278px] md:w-[540px] md:h-[540px] overflow-hidden rounded-2xl border-b-2 border-b-[#079CDE] shadow-[0_0_20px_#079CDE]">
+        <div className="relative w-1/3 min-w-[180px] h-[278px] md:w-[540px] md:h-[540px] overflow-hidden rounded-2xl border-b-2 border-b-[#079CDE] shadow-[0_0_20px_#079CDE]">
           <video
             src="/gif/robot.mp4"
             autoPlay
