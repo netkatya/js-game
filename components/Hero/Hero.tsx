6075af7edefdfb03ba16b3a3cb3b05302c6e5e58
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-10-19 13:12:48 +0100

    fix

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -34,8 +34,8 @@ export default function Hero() {
 
   return (
     <section className="pt-[40px] md:pt-[80px] pb-[60px]">
-      <div className="flex items-center justify-center gap-[32px] md:gap-[40px] pl-[20px] pr-[20px]">
-        <div className="relative w-1/3 w-[140px] h-[215px] md:w-[540px] md:h-[540px] overflow-hidden rounded-2xl border-b-2 border-b-[#079CDE] shadow-[0_0_20px_#079CDE]">
+      <div className="flex items-center justify-center gap-[24px] md:gap-[40px] pl-[10px] pr-[10px] md:pl-[20px] md:pr-[20px]">
+        <div className="relative w-1/3 w-[180px] h-[280px] md:w-[540px] md:h-[540px] overflow-hidden rounded-2xl border-b-2 border-b-[#079CDE] shadow-[0_0_20px_#079CDE]">
           <video
             src="/gif/robot.mp4"
             autoPlay
@@ -48,8 +48,8 @@ export default function Hero() {
           </video>
         </div>
 
-        <div className="font-semibold text-center w-[561px] h-[446px] mb-[40px] md:w-[561px] md:h-[446px] w-auto h-auto">
-          <div className="w-[140px] h-[215px] md:w-[513px] md:h-[200px]">
+        <div className="font-semibold text-center w-[561px] h-[446px] mb-0 md:mb-[40px] md:w-[561px] md:h-[446px] w-auto h-auto">
+          <div className="w-[140px] h-[190px] md:w-[513px] md:h-[200px]">
             <TextType
               text={[
                 `Hello, human...\nMy name is A.R.I. — Artificial Robotic Instructor.\nI know JavaScript, but I cannot think like a programmer.\nWill you teach me?`,
@@ -58,23 +58,23 @@ export default function Hero() {
               pauseDuration={1500}
               showCursor={true}
               cursorCharacter="|"
-              className="text-[16px] md:text-3xl text-white whitespace-pre-wrap font-semibold"
+              className="text-[15px] md:text-3xl text-white whitespace-pre-wrap font-semibold"
             />
           </div>
-          <ul className="flex flex-col items-center gap-[8px] md:gap-6 font-semibold text-[40px] text-center mt-[10px] md:mt-10">
-            <li className="w-[120px] md:w-[543px]">
+          <ul className="flex flex-col items-center gap-[8px] md:gap-6 font-semibold text-[40px] text-center mt-0 md:mt-10">
+            <li className="w-[140px] md:w-[543px]">
               <Link
                 href="quiz/levelOne"
                 onClick={startNewGameClick}
                 className="block w-full p-2 md:p-3 text-center border-2 md:border-3 border-solid border-[#079cde] rounded-md 
-       text-base md:text-[40px] transition-shadow duration-300 hover:shadow-[0_0_20px_#079CDE]"
+       text-[15px] md:text-[40px] transition-shadow duration-300 hover:shadow-[0_0_20px_#079CDE]"
               >
                 Start training
               </Link>
             </li>
 
             <li
-              className={`w-[120px] md:w-[543px] ${
+              className={`w-[140px] md:w-[543px] ${
                 !progress || progress?.lastActiveLevel === "Completed"
                   ? "opacity-50 pointer-events-none"
                   : ""
@@ -83,7 +83,7 @@ export default function Hero() {
               <Link
                 href={continueLink}
                 className="block w-full p-2 md:p-3 text-center border-2 md:border-3 border-solid border-[#079cde] rounded-md 
-       text-base md:text-[40px] transition-shadow duration-300 hover:shadow-[0_0_20px_#079CDE]"
+       text-[15px] md:text-[40px] transition-shadow duration-300 hover:shadow-[0_0_20px_#079CDE]"
               >
                 Continue
               </Link>
