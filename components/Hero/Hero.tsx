1b9a79d8b35d2500d18b304c5d21bc88778b3893
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-10-20 09:16:39 +0100

    modif hero

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -34,8 +34,8 @@ export default function Hero() {
 
   return (
     <section className="pt-[40px] md:pt-[80px] pb-[20px] md:pb-[60px]">
-      <div className="flex items-center justify-center gap-[24px] md:gap-[40px] pl-[10px] pr-[10px] md:pl-[20px] md:pr-[20px]">
-        <div className="relative w-1/3 w-[180px] h-[278px] md:w-[520px] md:h-[520px] overflow-hidden rounded-2xl border-b-2 border-b-[#079CDE] shadow-[0_0_20px_#079CDE]">
+      <div className="flex flex-col md:flex-row items-center justify-center gap-[24px] md:gap-[40px] pl-[10px] pr-[10px] md:pl-[20px] md:pr-[20px]">
+        <div className="relative w-1/3 w-[290px] h-[290px] md:w-[520px] md:h-[520px] overflow-hidden rounded-2xl border-b-2 border-b-[#079CDE] shadow-[0_0_20px_#079CDE]">
           <video
             src="/gif/robot.mp4"
             autoPlay
@@ -49,7 +49,7 @@ export default function Hero() {
         </div>
 
         <div className="font-semibold text-center w-[561px] h-[446px] mb-0 md:mb-[40px] md:w-[561px] md:h-[446px] w-auto h-auto">
-          <div className="w-[140px] h-[190px] md:w-[543px] md:h-[200px]">
+          <div className="w-[290px] h-[130px] md:w-[543px] md:h-[200px]">
             <TextType
               text={[
                 `Hello, human...\nMy name is A.R.I. — Artificial Robotic Instructor.\nI know JavaScript, but I cannot think like a programmer.\nWill you teach me?`,
@@ -62,7 +62,7 @@ export default function Hero() {
             />
           </div>
           <ul className="flex flex-col items-center gap-[8px] md:gap-6 font-semibold text-[40px] text-center mt-0 md:mt-10">
-            <li className="w-[140px] md:w-[543px]">
+            <li className="w-[290px] md:w-[543px]">
               <Link
                 href="quiz/levelOne"
                 onClick={startNewGameClick}
@@ -74,7 +74,7 @@ export default function Hero() {
             </li>
 
             <li
-              className={`w-[140px] md:w-[543px] ${
+              className={`w-[290px] md:w-[543px] ${
                 !progress || progress?.lastActiveLevel === "Completed"
                   ? "opacity-50 pointer-events-none"
                   : ""
